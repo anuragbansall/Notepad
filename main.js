fdd6ef2abcc0cc3ae71d3f38664916e2463c3d35
Anurag Bansal <devanuragbansal@gmail.com>
2025-07-27 19:57:54 +0530

    Initialize project with Electron setup, including main files and .gitignore

---
+++ main.js
@@ -0,0 +1,19 @@
+// main.js
+import { app, BrowserWindow } from "electron";
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
